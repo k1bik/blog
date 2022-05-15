e8e533ca0eed87cf8aec75996a2c5e8e05318f1f
Kirill Biller <biller.k@yandex.ru>
2022-05-15 22:08:40 +0300

    create CRUD

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,48 @@
 class QuestionsController < ApplicationController
+	before_action :set_question, only: %i[edit update show destroy]
+
 	def index
 		@questions = Question.all
+	end
+
+	def new 
+		@question = Question.new
+	end
+
+	def create
+		@question = Question.new question_params
+
+		if @question.save
+			redirect_to questions_path
+		else
+			render :new
+		end
+	end
+
+	def update 
+		if @question.update question_params
+			redirect_to questions_path
+		else
+			render :new 
+		end
+	end
+
+	def show; end
+
+	def edit;	end
+
+  def destroy
+    @question.destroy
+    redirect_to questions_path
+  end
+
+	private 
+
+	def question_params
+		params.require(:question).permit(:title, :body)
+	end
+
+	def set_question
+		@question = Question.find(params[:id])
 	end
 end
