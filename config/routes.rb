9e582b1dd32bc1c8810d0bd0496e18cbd354d0c6
Kirill Biller <biller.k@yandex.ru>
2022-08-21 12:18:13 +0300

    add user registration without devise

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@
 Rails.application.routes.draw do
   root 'pages#index'
 
+  resources :users, only: %i[new create]
+
   resources :questions do
     resources :answers, except: %i[show new]
   end
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_220_703_165_412) do
+ActiveRecord::Schema[7.0].define(version: 20_220_821_082_733) do
   create_table 'answers', force: :cascade do |t|
     t.text 'body'
     t.integer 'question_id', null: false
@@ -26,6 +26,15 @@ ActiveRecord::Schema[7.0].define(version: 20_220_703_165_412) do
     t.text 'body'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
+  end
+
+  create_table 'users', force: :cascade do |t|
+    t.string 'email', null: false
+    t.string 'name'
+    t.string 'password_digest'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['email'], name: 'index_users_on_email', unique: true
   end
 
   add_foreign_key 'answers', 'questions'
